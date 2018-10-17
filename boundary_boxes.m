16197e536bbe5066fd7af7aad36eacbd8bfad578
benfidlers <43848896+benfidlers@users.noreply.github.com>
2018-10-17 12:10:40 +0200

    Update boundary_boxes.m

--- boundary_boxes.m
+++ boundary_boxes.m
@@ -1,19 +1,18 @@
 %% Extract features
-[labeled, numObjects] = bwlabel(Iopenned, 4);
+[labeled, numObjects] = bwlabel(Ifilled, 4);
 stats = regionprops(labeled,'Eccentricity', 'Area', 'BoundingBox');
-areas = [stats.Area];
 eccentricities = [stats.Eccentricity];
-boundingbox = [stats.BoundingBox];
 
 %% Use feature analysis to count objects
 idxOfObjects = find(eccentricities);
-statsDefects = stats(idxOfObjects);
 
 figure, imshow(I);
+hold on;
 for idx = 1 : length(idxOfObjects)
-    h = rectangle ('Position', statsDefects(idx).BoundingBox);
+    h = rectangle ('Position', stats(idx).BoundingBox);
     set(h,'EdgeColor',[.75 0 0]);
     set(h,'LineWidth',2);
 end
 
 title(['There are ', num2str(numObjects), ' objects in the picture']);
+hold off;
