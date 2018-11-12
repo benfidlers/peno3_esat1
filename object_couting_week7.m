d37f0aa025d891bae928a1d43c34c47d265dddf9
SimonGulix <simongulix@hotmail.com>
2018-11-12 16:05:57 +0100

    merge

--- object_couting_week7.m
+++ object_couting_week7.m
@@ -63,17 +63,19 @@ title("Boundary box + removed objects within objects, Number of objects = "+ nb_
 function [updated_corner_points, nb_of_groups] = remove_corner_points_within_corner_points(corner_points, nb_groups)
     mat_size = size(corner_points);
     groups = mat_size(2); % This is the original number_of_groups
-    nb_of_groups = nb_groups;
+    nb_of_groups = nb_groups; % Number of groups after regrouping
     updated_corner_points = corner_points;
     for first=1:groups
         % Loop through every group
         % Now draw boundary box
+        
         min_row_first = corner_points(1,first);
         min_col_first = corner_points(2,first);
         max_row_first = corner_points(3,first);
         max_col_first = corner_points(4,first);
+        
         for second = 1:groups
-            if first ~= second && max_row_first ~= 0 && corner_points(4, second) ~= 0 % If the max values would be 0, this won't be a group
+            if first ~= second && corner_points(:, first) ~= zeros(4,1) && corner_points(:, second) ~= 0 % If the max values would be 0, this won't be a group
                 % Same groups, cant lay within eachother
                 min_row_second = corner_points(1,second);
                 min_col_second = corner_points(2,second);
