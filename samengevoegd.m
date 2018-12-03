cb13eb9ca5a2311a0d27bf3e0ccc45e6a3480c3c
SimonGulix <simongulix@hotmail.com>
2018-12-03 17:03:44 +0100

    simon

--- samengevoegd.m
+++ samengevoegd.m
@@ -1,4 +1,4 @@
-
+clearvars
 h = 900;
 
 min_y = 120;
@@ -86,12 +86,10 @@ image(new_RGB);
 %new_depth = threshold(new_depth, min_thresh, max_thresh);
 subplot(1,3,3), imagesc(new_depth);
 
-%% simon
-
+%% SIMON: Color handling
 img = new_RGB;
 
-
-%img = imread('kinect/foto_x2_RGB_4.png'); % Load picture (1080 rows * 1920 col)
+% Declaring static variables
 THRESHOLD_VALUE = 2;
 
 MIN_ROW_LINES_BETWEEN_GROUPS = 15; %25 %15
@@ -105,64 +103,38 @@ SAME_PIXELS_SEARCH_GRID_SIZE = 10;%25
 GROUP_SEARCH_GRID_SIZE = 15; %25
 % Grid size = this variable * 2, it searches for pixels with a group number
 % (not 0) in this grid.
-SURROUDING_PERCENTAGE = 10;% %
+SURROUDING_PERCENTAGE = 10;
 MIN_NB_SURROUNDING_PIXELS = floor((SAME_PIXELS_SEARCH_GRID_SIZE * 2)^2 * SURROUDING_PERCENTAGE/100) ;%125 % 50
 % The minimum number of pixels with the same value that are in the grid
 % size defined by SAME_PIXELS_SEARCH_GRID
 % The pixels that have a less number of surrounding pixels, are not defined
 % as a group but as noise.
 
-% CROPPING: Defining rectangle
-%top_row = 290 ; top_col = 760; bottom_row = 690 ; bottom_col = 1440;
-%top_row = 150; top_col = 750; bottom_row = 950; bottom_col = 1900;
-%top_row = 200; top_col = 850; bottom_row = 750; bottom_col = 1850; % For pictues with x2_RGB_... in name
-%top_row = 100, top_col = 100; bottom_row = 980; bottom_col = 1820; % For pictues with RGB in name.
-
 tic
 disp("Step 1: loading the image...");
-disp("Minimum distance between 2 objects (only straight vertical or straight horizontal = " + max([MIN_ROW_LINES_BETWEEN_GROUPS SAME_PIXELS_SEARCH_GRID_SIZE MIN_NB_SURROUNDING_PIXELS;]) + " pixels");
-
 disp("Step 2: converting the image to greyscale...");
-
 A = greyscale(img); % Convert image to grayscale
-
-
-%top_left_row, top_left_col, bottom_right_row, bottom_right_col
 disp("Step 3: cropping the image...");
-
-%A = simon_crop(A, top_row, top_col, bottom_row, bottom_col);
 imshow(A, []);
-%%
-%A = simon_crop(A, 100,100,980,1820, 1); % USE FOR foto RGB X
-%A = simon_crop(A, top_row,top_col,bottom_row, bottom_col,1); % USE FOR foto XX RGB
-
 disp("Step 4: blurring the image...");
 A = gaussian_blur(mean_blur(A)); % Filters
-% Method 3: First greyscale, then blur, then edge detect then threshold and then noise removal
 disp("Step 5: edge detecting...");
 first_edge_detect = edge_detect(A); % Laplacian edge detection
 disp("Step 6: thresholding edge");
 without_noise_removal = threshold_edge(remove_boundary(first_edge_detect, 15), THRESHOLD_VALUE); % Remove boundary around image & threshold the edges.
 disp("Step 7: noise removing...");
-%with_noise_removal = noise_deletion(without_noise_removal,5); % Noise removal
 with_noise_removal = without_noise_removal;
 disp("Step 8: grouping...");
 [grouped, nb_of_groups] = group(~with_noise_removal, SAME_PIXELS_SEARCH_GRID_SIZE, GROUP_SEARCH_GRID_SIZE, MIN_NB_SURROUNDING_PIXELS); % Group pixels together
 disp("Step 9: regrouping...");
 [regrouped, nb_of_groups2] = regroup(grouped, nb_of_groups, MIN_ROW_LINES_BETWEEN_GROUPS); % Regroup (nessicary because group function works from top left to bottom right
-
 %Find corner points of object (not really corner points on the boundary,
 %but corner points for the boundary box)
 disp("Step 10: calculating corner points...");
 corner_points = find_corner_points(regrouped, nb_of_groups); % Make sure to use nb_of_groups and not groups 2 because some groups don't exist anymore!
-
 disp("Step 11: removing objects within objects...");
-%[updated_corner_points, nb_of_groups3] = remove_corner_points_within_corner_points(corner_points, nb_of_groups2); % To remove objects within objects
-[updated_corner_points, nb_of_groups3] = remove_box_edge(corner_points, nb_of_groups2);
-[updated_corner_points, nb_of_groups3] = remove_corner_points_within_corner_points(updated_corner_points, nb_of_groups3);
-%updated_corner_points = corner_points;
-%nb_of_groups3 = nb_of_groups2;
-
+[updated_corner_points, nb_of_groups3] = remove_box_edge(corner_points, nb_of_groups2); % Remove the boundary box of the box.
+[updated_corner_points, nb_of_groups3] = remove_corner_points_within_corner_points(updated_corner_points, nb_of_groups3); % Remove boundary boxes fully inside other boundary boxes.
 disp("Step 12: drawing boundary boxes...");
 boundary_box = draw_boundary_box(A, updated_corner_points);
 disp("Step 13: drawing red boundary boxes on full image...");
@@ -1394,7 +1366,9 @@ function usefull_matrix = crop_depth_to_basket(depth_img, original_img)
 
 end
 
-
+function nextStep(src,event)
+    disp("ok");
+end
 
 
 
